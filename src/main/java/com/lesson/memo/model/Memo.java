738e0beea3cdcc81b800464384e06c2ec794e8bf
nakamurayuka <nakamurayuka@localhost>
2026-03-06 14:07:24 +0900

    優先度を選択必須に修正

--- src/main/java/com/lesson/memo/model/Memo.java
+++ src/main/java/com/lesson/memo/model/Memo.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 enum Priority {
@@ -45,6 +46,7 @@ public class Memo {
     @Column(nullable = false, length = 1000)
     private String content;
     
+    @NotNull(message = "優先度を選択してください")
     private Priority priority;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
